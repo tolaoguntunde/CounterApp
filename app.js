5b3e8fc87a53eff4f7e9f24d6daff28e9efe723e
tolaoguntunde <50242126+tolaoguntunde@users.noreply.github.com>
2021-12-14 01:05:45 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const value = document.querySelector('#counter-value');
+const btn = document.querySelectorAll('.btn');
+let count = 0;
+
+btn.forEach(element => { element.addEventListener('click',check)});
+
+
+function check (event) {
+    const checkTarget=event.currentTarget.classList;
+   if(checkTarget.contains('decrease')){
+       count-- ; 
+   } 
+   else if(checkTarget.contains('increase')){
+       count++ ; 
+   }
+else if(checkTarget.contains('reset')){
+   count = 0;
+   }
+   value.innerText = count;
+   if (count > 0){
+    value.style.color = 'green';
+}
+    else if (count < 0){
+        value.style.color = 'red';
+    }
+
+
+}
+
+
+   
